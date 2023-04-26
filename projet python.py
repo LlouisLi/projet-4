fb7fe9fd1943ae314d613004ee54783825448b6d
Louis <113100108+LlouisLi@users.noreply.github.com>
2023-04-27 00:13:16 +0200

    web scraping

--- projet python.py
+++ projet python.py
@@ -1,3 +1,15 @@
 import subprocess
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import os
 
-subprocess.run(r"C:\Program Files\Notepad++\notepad++.exe")
+#subprocess.run("gmail.com")
+
+url = "https://accounts.google.com/AccountChooser/signinchooser"
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
+print(soup)
+for i in soup:
+    print (i)
