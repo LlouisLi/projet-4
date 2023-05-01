e0f7dc1f800d89fe69f7eb7ffce68a4efe7bc3e8
Louis <113100108+LlouisLi@users.noreply.github.com>
2023-05-01 18:38:36 +0200

    combinaison des 2 codes

--- projet python.py
+++ projet python.py
@@ -1,15 +1,28 @@
-import subprocess
+#import subprocess
 import requests
-import urllib.request
-from bs4 import BeautifulSoup
-import os
-import pandas as pd
+import urllib.request #permettent de faire des requetes
+from bs4 import BeautifulSoup #rendre pages accessibles
+import pandas as pd #pour mettre l'objet dans un table
+from selenium import webdriver #permettra de tansformer le 
+#code dans "inspecter de la page en html"
+from time import sleep #repos 
 
-#subprocess.run("gmail.com")
+def connection():
+    url = "https://mail.google.com/mail/u/0/#sent"
+    css_selector = ".zA yO" #id de la ligne du tableau avec l'adresse mail
 
-url = "https://mail.google.com"
-response = requests.get(url)
+    wd_path = "PROJET ER/chromedriver" #chromedriver: pouvoir interagir avec la page google
+
+    wdriver = webdriver.Chrome(executable_path = wd_path) 
+
+    wdriver.get(url)
+    #subprocess.run("gmail.com")
+    return wdriver
+
+url = "https://mail.google.com/mail/u/0/#sent"
+response = requests.get(connection)
 soup = BeautifulSoup(response.text,"html.parser")
 
-Gibaud = soup.find(attrs={'class':"zA zE"})
-print(Gibaud)
+Gibaud = soup.findAll("td")
+for i in Gibaud:
+    print(i["class"])
